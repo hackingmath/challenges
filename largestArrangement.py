fae08f1ecf877141fcfec1f1f84014712d4863fc
Peter Farrell <farrellpolymath@gmail.com>
2018-11-01 20:57:33 -0700

    Add files via upload

--- largestArrangement.py
+++ largestArrangement.py
@@ -3,10 +3,16 @@ https://www.codewars.com/kata/59d902f627ee004281000160
 October 30, 2018'''
 
 def str_sort(arr):
+    '''Sorts a list of integers to return
+    the highest integer'''
+    #if the array has 10 items, sort 9 times
     for j in range(len(arr)-1):
+        #compare every item with the next one
         for i in range(len(arr)-1):
-            if int(str(arr[i])+str(arr[i+1])) < \
-               int(str(arr[i+1])+str(arr[i])):
+            #the two strings in order in the list
+            a,b = arr[i], arr[i+1]
+            #if ab is less than ba, switch them
+            if int(str(a)+str(b)) < int(str(b)+str(a)):
                 arr[i],arr[i+1] = arr[i+1],arr[i]
             print(arr)
     return arr
