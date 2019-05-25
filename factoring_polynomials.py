e885e7fd13fd2f227ad33b22962e387d9d3422d5
Peter Farrell <farrellpolymath@gmail.com>
2019-05-24 18:43:39 -0700

    Add files via upload

--- factoring_polynomials.py
+++ factoring_polynomials.py
@@ -3,7 +3,8 @@ May 22, 2019
 From Hacking Math Class with Python"""
 
 import time
-start_time = time.time()
+from math import sqrt
+start_time = time.perf_counter_ns()
 
 def factors(n):
     """Returns a list of the factors of n"""
@@ -25,25 +26,58 @@ def factor_polynomial(a,b,c):
             f = a/d
             e = c/g
             if e*f + d*g == b:
-                return True
-    return False
+                print("{}*x**2 + {}*x + {} = ({}x + {})({}x + {})".format(a,b,c,d,int(e),int(f),g))
+                return
+    #return False
 
-n = 0
-factorable = 0
+def is_discriminant_square(a,b,c):
+    """a,b,c, are coeffs of a*x**2 + b*x + c. Returns True if
+    discriminant is a perfect square."""
+    disc = b**2 - 4*a*c
+    if disc < 0:
+        return False
+    return sqrt(disc) == int(sqrt(disc))
 
-NUMS = list(range(-20,0)) + list(range(1,21))
+##print(is_discriminant_square(4,20,25)) #4*x**2 + 20*x + 25 = (2x + 5)**2
+##print(is_discriminant_square(1,5,6)) #not a perfect square, but factorable
+##print(is_discriminant_square(4,-11,5)) #not factorable
 
-for a in NUMS:
-    for b in NUMS:
-        for c in NUMS:
-            n += 1            
-            if factor_polynomial(a,b,c):
-                factorable += 1
-            
-print("Number of trinomials:",n)
-print("Factorable: ",factorable)
-print("Percentage:",factorable/n*100)
+for i in range(2):
+    start_time = time.perf_counter_ns()
+    n = 0
+    factorable = 0
 
-print("Time (secs):",round(time.time()-start_time,1))
+    NUMS = list(range(-10,0)) + list(range(1,11))
+
+    for a in NUMS:
+        for b in NUMS:
+            for c in NUMS:
+                factor_polynomial(a,b,c)
+
+    '''for a in NUMS:
+        for b in NUMS:
+            for c in NUMS:
+                n += 1
+                if i == 0:
+                    if factor_polynomial(a,b,c):
+                        factorable += 1
+                else:
+                    if is_discriminant_square(a,b,c):
+                        factorable += 1
+    print()
+    if i == 0:
+        print("Factoring function:")
+    else:
+        print("Checking Discriminant:")
+        
+    print("Number of trinomials:",n)
+    print("Factorable: ",factorable)
+    print("Percentage:",factorable/n*100)'''
+
+    end = time.perf_counter_ns()
+    
+    print("Time (secs):",round((end-start_time)/1000000000,2))
+
+    
 #Factor 6*x**2 + x - 12. Spoiler alert: it's (2*x + 3)*(3*x - 4)
-#factor_polynomial(6,1,-12)
+#factor_polynomial(6,1,-12)'''
